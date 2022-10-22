30c59e4ef596adc1241aef7ce369d92c8c2b8ff0
vratant11 <vratant2012061@akgec.ac.in>
2022-10-22 20:06:47 +0530

    Image changed

--- src/Components/Image.jsx
+++ src/Components/Image.jsx
@@ -4,21 +4,18 @@ import "./imagesection.css";
 const Image = () => {
   return (
     <div className="imagebox">
-      
-        <img className="image"
-          src="https://media.gettyimages.com/photos/stack-of-books-picture-id157482029?s=612x612"
-          alt="Error"
-        />
-      
-      
-        <p className="text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis odio
-          ad accusantium dicta reprehenderit maiores velit nemo, facilis modi,
-          ducimus eveniet suscipit inventore, quisquam veniam aspernatur. Cumque
-          impedit illo laboriosam.
-         
-        </p>
-      
+      <img
+        className="image"
+        src="https://images.unsplash.com/photo-1603302576837-37561b2e2302?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGxhcHRvcHxlbnwwfHwwfHw%3D&w=1000&q=80"
+        alt="Error"
+      />
+
+      <p className="text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis odio
+        ad accusantium dicta reprehenderit maiores velit nemo, facilis modi,
+        ducimus eveniet suscipit inventore, quisquam veniam aspernatur. Cumque
+        impedit illo laboriosam.
+      </p>
     </div>
   );
 };
