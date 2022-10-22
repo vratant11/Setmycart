5af0ad0eb01c537f80de333f2f8749dc595fa1c4
vratant11 <vratant2012061@akgec.ac.in>
2022-10-22 20:10:28 +0530

    Image changed

--- src/Components/Crousel.jsx
+++ src/Components/Crousel.jsx
@@ -6,7 +6,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "./crousel.css";
 
 // import required modules
-import { Pagination, Navigation,Autoplay } from "swiper";
+import { Pagination, Navigation, Autoplay } from "swiper";
 const Crousel = () => {
   return (
     <>
@@ -15,21 +15,21 @@ const Crousel = () => {
           type: "progressbar",
         }}
         navigation={true}
-        modules={[Pagination, Navigation,Autoplay]}
+        modules={[Pagination, Navigation, Autoplay]}
         autoplay={{ delay: 3500 }}
         className="mySwiper"
       >
         <SwiperSlide>
           <img src="/assets/slide1.jpg" alt="Error" />
-
         </SwiperSlide>
         <SwiperSlide>
           <img src="/assets/slide3.jpg" alt="Error" />
-
         </SwiperSlide>
         <SwiperSlide>
-          <img src="/assets/slide2.jpg" alt="Error" />
-
+          <img
+            src="https://www.thoughtco.com/thmb/NRuMaaVBhsrz3AyDBweiPAZpYfw=/1500x844/smart/filters:no_upscale()/GettyImages-909076272-5c48c8c146e0fb0001891c02.jpg"
+            alt="Error"
+          />
         </SwiperSlide>
       </Swiper>
     </>
